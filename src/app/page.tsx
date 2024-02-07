88c08f39c222ea279399d3d065953f37e989de61
xXQiuChenXx <lautaihong2004@gmail.com>
2024-02-08 00:19:45 +0800

    live cursor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@ import { CollaborativeApp } from "../components/CollaborativeApp";
 
 export default function Page() {
   return (
-    <div className="h-[100vh] w-full items-center justify-center text-center flex">
-      <h1 className="text-2xl text-white">Liveblocks Figma Clone</h1>
-
+    <div>
       <Live />
     </div>
   );
--- src/components/Live.tsx
+++ src/components/Live.tsx
@@ -1,13 +1,53 @@
 "use client";
-import { useOthers } from "../../liveblocks.config";
+import { PointerEvent, useCallback } from "react";
+import { useMyPresence, useOthers } from "../../liveblocks.config";
 import LiveCursors from "./cursor/LiveCursors";
 
 const Live = () => {
   const others = useOthers();
-  
+  const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+
+  const handlePointerMove = useCallback((event: PointerEvent) => {
+    event.preventDefault();
+
+    const boundingClient = event.currentTarget.getBoundingClientRect();
+
+    const x = event.clientX - boundingClient.x;
+    const y = event.clientY - boundingClient.y;
+
+    updateMyPresence({ cursor: { x, y } });
+  }, []);
+
+  const handlePointerLeave = useCallback((event: PointerEvent) => {
+    event.preventDefault();
+
+    const boundingClient = event.currentTarget.getBoundingClientRect();
+
+    const x = event.clientX - boundingClient.x;
+    const y = event.clientY - boundingClient.y;
+
+    updateMyPresence({ cursor: null, message: null });
+  }, []);
+
+  const handlePointerDown = useCallback((event: PointerEvent) => {
+    const boundingClient = event.currentTarget.getBoundingClientRect();
+
+    const x = event.clientX - boundingClient.x;
+    const y = event.clientY - boundingClient.y;
+
+    updateMyPresence({ cursor: { x, y } });
+  }, []);
+
   return (
-    <div>
-      <LiveCursors others={others}/>
+    <div
+      onPointerMove={handlePointerMove}
+      onPointerDown={handlePointerDown}
+      onPointerLeave={handlePointerLeave}
+      className="h-[100vh] w-full items-center justify-center text-center flex"
+    >
+      <h1 className="text-2xl text-white">Liveblocks Figma Clone</h1>
+
+      <LiveCursors others={others} />
     </div>
   );
 };
