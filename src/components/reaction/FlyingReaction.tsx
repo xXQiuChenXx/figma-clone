5746d3b2b437836b08874003347575f244c6a4c8
xXQiuChenXx <lautaihong2004@gmail.com>
2024-02-21 22:54:12 +0800

    update

---
+++ src/components/reaction/FlyingReaction.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FlyingReaction = () => {
+  return (
+    <div>FlyingReaction</div>
+  )
+}
+
+export default FlyingReaction
---
+++ src/components/reaction/ReactionButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ReactionButton = () => {
+  return (
+    <div>ReactionButton</div>
+  )
+}
+
+export default ReactionButton
