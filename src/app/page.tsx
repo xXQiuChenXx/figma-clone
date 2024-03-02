09e304b1b3bbe66acceee9ce8bdd23d2a02ddd92
xXQiuChenXx <lautaihong2004@gmail.com>
2024-03-02 20:52:45 +0800

    sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
+import LeftSidebar from "@/components/LeftSidebar";
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
+import RightSidebar from "@/components/RightSidebar";
 
 export default function Page() {
   return (
     <main className="h-screen overflow-hidden">
       <Navbar />
       <section className="flex flex-row h-full ">
+        <LeftSidebar />
         <Live />
+        <RightSidebar />
       </section>
     </main>
   );
